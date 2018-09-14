8747108d7bfea2f4a8ff4901893472081436b565
Eric Shih <eric.shih@nexusguard.com>
2018-09-14 17:49:27 +0800

    將變數拉到.env file 讀取

--- src/parser.js
+++ src/parser.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const fs = require('fs');
 const _ = require('lodash');
 const webhook = require('./webhook');
+require('dotenv').config();
 
-const url = 'https://rent.591.com.tw/home/search/rsList?is_new_list=1&type=1&kind=1&searchtype=1&region=1&section=5,10,8,3,2&rentprice=3&pattern=0&area=0,0&not_cover=1';
+const { RENT_INFO_URL, COOKIE_INFO } = process.env;
 
 const get591JSON = async () => {
-  const result = await axios.get(url, {
+  const result = await axios.get(RENT_INFO_URL, {
     headers: {
-      Cookie: 'urlJumpIp=1;urlJumpIpByTxt=%E5%8F%B0%E5%8C%97%E5%B8%82;',
+      Cookie: COOKIE_INFO,
     },
   });
   return result;
--- src/webhook.js
+++ src/webhook.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+require('dotenv').config();
+
+const { WEBHOOK_EVENT, WEBHOOK_ID } = process.env;
 
 module.exports = {
   sendWebhook: async (contentJSON) => {
-    await axios.post('https://maker.ifttt.com/trigger/591/with/key/CkWt5SLK8Updi48485YE0', contentJSON);
+    await axios.post(`https://maker.ifttt.com/trigger/${WEBHOOK_EVENT}/with/key/${WEBHOOK_ID}`, contentJSON);
   },
 };
